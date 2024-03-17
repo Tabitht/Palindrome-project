93b421d53f173f8690fa9a67ae2fdf4e671796d6
Tabitht <imafidontabitha@gmail.com>
2024-03-17 14:09:16 +0100

    Created a new folder and worked on the Css and Html of the project

---
+++ Palindrome checker/javascript.js
@@ -0,0 +1,6 @@
+// checks if a word or phrase is a palindrome
+function ispalindrome(phrase){
+    //coverts the string to lowercase and removes non-alphanumeric characters
+    let word = phrase.tolowerCase().replace(/[^a-z0-9]/g, '');
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Palindrome-project
 A simple palindrome website
+The project is a palindrome checker, it checks if a word is a palindrome and outputs the inputted word
