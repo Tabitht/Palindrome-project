8c9f69cd8a12588628146bdef81b79f3a0f86000
Tabitht <imafidontabitha@gmail.com>
2024-03-22 20:51:53 +0100

    Added javascript functionality

--- Palindrome checker/javascript.js
+++ Palindrome checker/javascript.js
@@ -1,6 +1,22 @@
-// checks if a word or phrase is a palindrome
-function ispalindrome(phrase){
+function convertPhrase(phrase){
     //coverts the string to lowercase and removes non-alphanumeric characters
-    let word = phrase.tolowerCase().replace(/[^a-z0-9]/g, '');
-
+    let word = phrase.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let palindromeWord = word.split('').reverse().join('');
+    return word === palindromeWord;
 }
+// checks if a word or phrase is a palindrome
+function isPalindrome(){
+    let sentence = document.querySelector("#word");
+    let enteredWord = document.querySelector("#enteredword");
+    let result = document.querySelector("#answer");
+    if (convertPhrase(sentence.value)){
+        enteredWord.innerHTML = `ENTERED WORD: ${sentence.value}`;
+        result.innerHTML = `The word "${sentence.value}" is a Palindrome`;
+    }
+    else{
+        enteredWord.innerHTML = `PALINDROME WORD: ${sentence.value}`;
+        result.innerHTML = `The word "${sentence.value}" is not a Palindrome`;
+    }
+}
+let applyButton = document.querySelector("#click");
+applyButton.addEventListener("click", isPalindrome);
